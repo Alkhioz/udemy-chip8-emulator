c6836adadd073baa6b39ba8d7776e1e91933dcc0
Alejandro Mendoza <33111241+Alkhioz@users.noreply.github.com>
2023-12-15 13:43:24 -0500

    Added function to draw pixels on the screen

--- src/main.c
+++ src/main.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
     struct chip8 chip8;
     chip8_init(&chip8);
 
-    chip8_screen_set(&chip8.screen, 10, 0);
+    chip8_screen_draw_sprite(&chip8.screen, 32, 30, &chip8.memory.memory[0x00], 5);
 
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window *window = SDL_CreateWindow(
